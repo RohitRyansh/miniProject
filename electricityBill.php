29053a685e5e5ccabd476aa4a4eca3f1c1e91753
rohit <rohitryanhs@gmail.com>
2022-08-26 09:59:13 +0530

    bill file

--- electricityBill.php
+++ electricityBill.php
@@ -1,28 +1,53 @@
 <?php
+function f1($bill)
+{
+    $sum=$bill*3.50;
+    return $sum;
+}
+function f2($bill)
+{
+    $temp=$bill-50;
+    $sum=f1(50);
+    $temp=$sum+$temp*4.00;
+    return $temp;
+}
+function f3($bill)
+{
+    $temp=$bill-150;
+    $sum=f2(150);
+    $temp=$sum+$temp*5.20;
+    return $temp;
+}
+function f4($bill)
+{
+    $temp=$bill-250;
+    $sum=f3(250);
+    $temp=$sum+$temp*6.50;
+    return $temp;
+} 
 function electricityBill($bill)
 {
-    $sum=0;
-    if($bill>250)
+    if($bill>=250)
     {
-        $sum=$bill*6.50;
+        $sum=f4($bill);
         return $sum;
     }
-    elseif($bill<=250 && $bill>150)
+    elseif($bill<250 && $bill>=150)
     {
-        $sum=$bill*5.20;
+        $sum=f3($bill);
         return $sum;
     }
-    elseif($bill<=150 && $bill>50)
+    elseif($bill<150 && $bill>=50)
     {
-        $sum=$bill*4.00;
+        $sum=f2($bill);
         return $sum;
     }
     else
     {
-        $sum=$bill*3.50;
+        $sum=f1($bill);
         return $sum;
-    }   
-}
-$total=electricityBill(166);
-echo "the total Electricity Bill is ".$total;
+    } 
+}  
+$total=electricityBill(55);
+echo "Total Electricty bill is ".$total;
 ?>
